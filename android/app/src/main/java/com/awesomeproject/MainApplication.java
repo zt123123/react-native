2934062db9bdf22f2aba15421a6dfae938e03298
ZhangTian <249812928@qq.com>
2018-10-14 13:53:38 +0800

    添加热更新代码

--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -3,6 +3,7 @@ package com.awesomeproject;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import cn.reactnative.modules.update.UpdatePackage;
 import com.theweflex.react.WeChatPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new UpdatePackage(),
             new WeChatPackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage(),
--- page/Home/HomePage.js
+++ page/Home/HomePage.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react';
 import { Platform, Dimensions,StyleSheet,PixelRatio,
     Alert,TouchableNativeFeedback,ScrollView, Image,
     ActivityIndicator, Text, FlatList, View ,
-    RefreshControl
+    RefreshControl,Linking
 } from 'react-native';
+
+import {
+  isFirstTime,
+  isRolledBack,
+  packageVersion,
+  currentVersion,
+  checkUpdate,
+  downloadUpdate,
+  switchVersion,
+  switchVersionLater,
+  markSuccess,
+} from 'react-native-update';
+
+import _updateConfig from '../../update.json';
 
 import Swiper from 'react-native-swiper';
 import LinearGradient from 'react-native-linear-gradient';
 
-const {width,height} = Dimensions.get('window');
-
 import {cmnProductList,cmnBannerList} from "../../utils/ajax"
 
 import ProductItem from "../../components/ProductItem"
+
+const {width,height} = Dimensions.get('window');
+const {appKey} = _updateConfig[Platform.OS];
 
 export default class HomePage extends Component {
 
@@ -29,7 +44,50 @@ export default class HomePage extends Component {
         this.renderMovie = this.renderMovie.bind(this);
         this._onRefresh = this._onRefresh.bind(this);
     }
+    componentWillMount(){
+        if (isFirstTime) {
+          Alert.alert('提示', '这是当前版本第一次启动,是否要模拟启动失败?失败将回滚到上一版本', [
+            {text: '是', onPress: ()=>{throw new Error('模拟启动失败,请重启应用')}},
+            {text: '否', onPress: ()=>{markSuccess()}},
+          ]);
+        } else if (isRolledBack) {
+          Alert.alert('提示', '刚刚更新失败了,版本被回滚.');
+        }
+    }
+
+    doUpdate(info){
+        downloadUpdate(info).then(hash => {
+          Alert.alert('提示', '下载完毕,是否重启应用?', [
+            {text: '是', onPress: ()=>{switchVersion(hash);}},
+            {text: '否',},
+            {text: '下次启动时', onPress: ()=>{switchVersionLater(hash);}},
+          ]);
+        }).catch(err => { 
+          Alert.alert('提示', '更新失败.');
+        });
+    }
+
+    checkUpdate(){
+        checkUpdate(appKey).then(info => {
+          if (info.expired) {
+            Alert.alert('提示', '您的应用版本已更新,请前往应用商店下载新的版本', [
+              {text: '确定', onPress: ()=>{info.downloadUrl && Linking.openURL(info.downloadUrl)}},
+            ]);
+          } else if (info.upToDate) {
+            Alert.alert('提示', '您的应用版本已是最新.');
+          } else {
+            Alert.alert('提示', '检查到新的版本'+info.name+',是否下载?\n'+ info.description, [
+              {text: '是', onPress: ()=>{this.doUpdate(info)}},
+              {text: '否',},
+            ]);
+          }
+        }).catch(err => { 
+          Alert.alert('提示', '更新失败.');
+        });
+    }
+
     componentDidMount() {
+        this.checkUpdate()
         this.getProductList()
         this.getBannerList()
     }
